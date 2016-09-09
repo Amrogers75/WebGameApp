7746ba00656103bd4a8c23ba70b2f5ca1a4be8c9
Thornton <tybone99@gmail.com>
2016-09-09 10:09:28 -0600

    setup

---
+++ Scripts/menus.js
@@ -0,0 +1,14 @@
+(function(){
+
+    angular.module('myApp')
+        .component('menu', { // the tag for using this is <char-detail>
+            templateUrl: "Templates/menu.html",
+            controller: menuController
+
+        });
+
+    function menuController() {
+        
+         }
+})();
+    
